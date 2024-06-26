05a3562daeb79043221ea07d1eaee9daa9502920
adam-godel <agodel@bu.edu>
2024-06-26 09:24:41 -0400

    Added website url and info

--- website/src/components/outro/outro.jsx
+++ website/src/components/outro/outro.jsx
@@ -6,7 +6,7 @@ export default function Outro() {
         <>
             <h1 style={{marginTop: "1rem"}}>Congratulations!</h1>
             <h2 style={{marginTop: "-0.2rem", marginBottom: "-0.2rem"}}>You just learned how to create the glued trees algorithm!</h2>
-            <p>You now have all the information you'll need to understand the Glued Trees notebook, which contains the actual code for the algorithm. You can access it by clicking the button below.</p>
+            <p>You now have all the information you need to understand the Glued Trees notebook, which contains the actual code for the algorithm. You can access it by clicking the button below.</p>
             <button onClick={() => window.open('notebook', "_self")}>Glued Trees Notebook</button>
             <br/><br/>
         </>
